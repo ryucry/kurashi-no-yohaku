00fca27cfa9dc2f8edeeffce521835f6934a36ab
ryucry <ai.ryucry@gmail.com>
2026-09-12 15:08:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,32 +13,6 @@ st.markdown("""
 }
 </style>
 
-/* 上部ヘッダー */
-.header {
-    padding: 25px 5% 10px 5%;
-    border-bottom: 1px solid #394044;
-}
-
-.logo {
-    font-size: 30px;
-    letter-spacing: 8px;
-    color: #ffffff;
-}
-
-.subtitle {
-    color: #8d999d;
-    font-size: 16px;
-    letter-spacing: 3px;
-}
-
-/* ナビゲーション */
-.nav-box {
-    margin-top: 25px;
-    padding: 12px 0;
-    color: #9ca8ac;
-    font-size: 16px;
-}
-import streamlit as st
 """, unsafe_allow_html=True)
 st.set_page_config(
     page_title="一人暮らし必需品",
